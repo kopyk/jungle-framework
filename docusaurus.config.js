bb814b7b47480f7c2d4ab4474228acfd939e0eb3
jordan yan <theconfusedgenuis@gmail.com>
2025-12-13 22:38:44 -0600

    changed domain name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kopyk.github.io',
+  url: 'http://jordanyan.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/jungle-framework/',
